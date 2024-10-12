a27b77cd72479121be5f7cef503ea44feb3210e4
oğuzhan <k__oguzzz@hotmail.com>
2024-10-12 14:59:41 +0300

    set up infrastructure project logger added email service added repository completed

--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetAllLeaveTypesQueryHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetAllLeaveTypesQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using MediatR;
 
@@ -8,15 +9,18 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveT
     {
         private readonly IMapper _mapper;
         private readonly ILeaveTypeRepository _leaveTypeRepository;
-        public GetAllLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        private readonly IAppLogger<GetAllLeaveTypesQueryHandler> _logger;
+        public GetAllLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository,IAppLogger<GetAllLeaveTypesQueryHandler> logger)
         {
             _mapper = mapper;
             _leaveTypeRepository = leaveTypeRepository;
+            _logger = logger;
         }
         public async Task<List<LeaveTypeDto>> Handle(GetAllLeaveTypesQuery request, CancellationToken cancellationToken)
         {
             var leaveTypes = await _leaveTypeRepository.GetAllAsync();
             var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+            _logger.LogInformation("Leave Types were retrieved successfully");
             return data;
         }
     }
---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -0,0 +1,57 @@
+﻿using HR.LeaveManagament.Domain;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
+    {
+        public LeaveAllocationRepository(HrDatabaseContext context) : base(context)
+        {
+        }
+
+        public async Task AddAllocations(List<LeaveAllocation> allocations)
+        {
+            await _context.AddRangeAsync(allocations);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> AllocationExist(string userId, int leaveTypeId, int period)
+        {
+            return await _context.LeaveAllocations.AnyAsync(x => x.EmployeeId == userId
+            && x.LeaveTypeId == leaveTypeId && x.Period == period);
+        }
+
+        public async Task<LeaveAllocation> GetLeaveAllocationWithDetails(int id)
+        {
+           var leaveAllocation = _context.LeaveAllocations
+                .Include(x => x.LeaveType)
+                .FirstOrDefault(x => x.Id == id);
+            return leaveAllocation;
+        }
+
+        public async Task<List<LeaveAllocation>> GetLeaveAllocationWithDetails()
+        {
+            var leaveAllocations = await _context.LeaveAllocations
+               .Include(x => x.LeaveType)
+               .ToListAsync();
+            return leaveAllocations;
+        }
+
+        public async Task<List<LeaveAllocation>> GetLeaveAllocationWithDetails(string userId)
+        {
+            var leaveAllocations = await _context.LeaveAllocations
+             .Where(x => x.EmployeeId == userId)
+              .Include(x => x.LeaveType)
+              .ToListAsync();
+            return leaveAllocations;
+        }
+
+        public async Task<LeaveAllocation> GetUserAllocations(string userId,int leaveTypeId)
+        {
+            return await _context.LeaveAllocations
+                .FirstOrDefaultAsync(x => x.EmployeeId == userId && x.LeaveTypeId == leaveTypeId);
+        }
+    }
+}
